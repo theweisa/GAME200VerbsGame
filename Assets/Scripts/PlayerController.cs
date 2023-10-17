4485c4002a3c01c740731cbcce3da4fb9f728bb4
theweisa <weiandrew2012@gmail.com>
2023-10-17 02:21:15 -0700

    physics n movement n stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using NaughtyAttributes;
+using UnityEditor.EditorTools;
+using Unity.VisualScripting;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum ActionType { Jump, Blow }
+    public Dictionary<ActionType, InputAction.CallbackContext> inputDict = new Dictionary<ActionType, InputAction.CallbackContext>();
+
+    [Header("References")]
+    [Space(4)]
+    public Rigidbody2D rb;
+    public Collider2D coll;
+    
+    [Header("Inspector Values")]
+    [Space(4)]
+    [Tooltip("How fast the player accelerates")]
+    public float moveAcceleration;
+    [Tooltip("Force Applied from Jumping")]
+    public float jumpForce;
+    [Tooltip("Force Applied from Blowing")]
+    public float blowForce;
+    [Tooltip("Max speed from moving")]
+    public float movementSpeedCap;
+    [Tooltip("Time before you can jump after jumping")]
+    public float fallMultiplier = 2f;
+    [Tooltip("Max possible velocity")]
+    public float velocityCap;
+    [Tooltip("Time you can jump while not grounded")]
+    public float coyoteTime = 0.2f;
+    private float coyoteTimeTimer = 0f;
+    [Tooltip("Time before you can jump after jumping")]
+    public float jumpBuffer = 0.1f;
+    private float jumpBufferTimer = 0f;
+    private float minJumpDist = 0.7f;
+
+    private Vector2 moveDirection;
+    private float baseGravityScale;
+    private float baseLinearDrag;
+    private bool jumped=false;
+    void Awake() {
+        rb = rb ? rb : Global.FindComponent<Rigidbody2D>(gameObject);
+        coll = coll ? coll : Global.FindComponent<Collider2D>(gameObject);
+    }
+    void Start()
+    {
+        baseLinearDrag = rb.drag;
+        baseGravityScale = rb.gravityScale;
+        //  Time.timeScale = 0.3f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimers();
+        UpdatePhysics();
+        ApplyMovement();
+    }
+    void ApplyMovement() {
+        if (Mathf.Abs(rb.velocity.x) > movementSpeedCap) return;
+        rb.AddForce(moveAcceleration*moveDirection);
+    }
+
+    #region Input Callbacks
+    public void Move(InputAction.CallbackContext context) {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+    public void Jump(InputAction.CallbackContext context) {
+        if (!ManageAction(ActionType.Jump, context)) return;
+        if (!CanJump()) return;
+        jumped = true;
+        coyoteTimeTimer = 0f;
+        jumpBufferTimer = jumpBuffer;
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+    }
+
+    public void Blow(InputAction.CallbackContext context) {
+        if (!ManageAction(ActionType.Blow, context)) return;
+        //rb.velocity = Vector2.zero;
+        rb.AddForce(Global.GetRelativeMousePosition(transform.position)*blowForce, ForceMode2D.Impulse);
+    }
+    #endregion
+
+    #region Helper Functions
+    void UpdateTimers() {
+        jumpBufferTimer = Mathf.Max(jumpBufferTimer-Time.deltaTime, 0f);
+        coyoteTimeTimer = Mathf.Max(coyoteTimeTimer-Time.deltaTime, 0f);
+    }
+    void UpdatePhysics() {
+        rb.velocity = velocityCap > 0 ? Vector2.ClampMagnitude(rb.velocity, velocityCap) : rb.velocity;
+        UpdateGrounded();
+        UpdateAirTime();
+    }
+    void UpdateGrounded() {
+        if (!IsGrounded()) return;
+        // if still able to jump
+        if (jumpBufferTimer <= 0f) {
+            rb.gravityScale = baseGravityScale;
+            rb.drag = baseLinearDrag;
+            coyoteTimeTimer = coyoteTime;
+            jumped = false;
+        }
+    }
+    void UpdateAirTime() {
+        if (IsGrounded()) return;
+        rb.drag = baseLinearDrag * 2f;
+        rb.gravityScale = baseGravityScale;
+        //rb.drag = baseLinearDrag * 0.15f;
+        if (rb.velocity.y < 0) {
+            rb.gravityScale = baseGravityScale*fallMultiplier;
+        }
+        else if (rb.velocity.y > 0f && jumped && !inputDict.ContainsKey(ActionType.Jump)) {
+            rb.gravityScale = baseGravityScale*fallMultiplier;
+        }
+    }
+    bool CanJump() {
+        return coyoteTimeTimer > 0f;
+    }
+    bool IsGrounded() {
+        return Physics2D.OverlapCircle(GetBottomPoint(), minJumpDist, 1 << LayerMask.NameToLayer("Environment"));
+    }
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the bottom point
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(GetBottomPoint(), minJumpDist);
+    }
+    bool ManageAction(ActionType action, InputAction.CallbackContext context) {
+        if (context.canceled) {
+            inputDict.Remove(action);
+            return false;
+        }
+        inputDict[action] = context;
+        if (context.started) return true;
+        return false;
+    }
+    private Vector2 GetBottomPoint() {
+        return new Vector2(coll.transform.position.x+coll.offset.x, coll.transform.position.y+coll.offset.y-coll.bounds.extents.y);
+    }
+    #endregion
+}
