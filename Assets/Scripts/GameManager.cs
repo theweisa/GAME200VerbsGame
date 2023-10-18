c0fe379d22c068b7176c9aa760ad12681af8ff78
Yiyang Lu <lyiyang@ucsc.edu>
2023-10-18 13:41:48 -0700

    Revert "Merge branch 'main' into multiplayer-branch"
    This reverts commit 872d24a28e925629916437d81495a7124e54b2a9, reversing
    changes made to d30f75ba46ebdcc68a6df74beb02e17a21b73668.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GameManager : UnitySingleton<GameManager>
 {
-    public Transform instanceManager;
     // Start is called before the first frame update
     void Start()
     {
