91a26efd05ea916ce1c14a5ce20649a2fa9b0ecb
jjctyu <70170991+jjctyu@users.noreply.github.com>
2023-10-19 21:21:10 -0700

    Just a simple menu

---
+++ Assets/Scripts/playbutton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class playbutton : MonoBehaviour
+{
+    public void OnPlayClick()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Quirgame()
+    {
+
+        Application.Quit();
+        Debug.Log("Yeah I am quit");
+    
+    }
+}
+
